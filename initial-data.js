e99d9c1cbc2413d4426c4febd28bf81cea6e966f
Lucie Berankova <lucie.berankova@2et.cz>
2026-07-28 10:32:48 +0200

    Update walking app 2026-07-28 10:32

--- initial-data.js
+++ initial-data.js
@@ -1,7 +1,7 @@
 window.MUZY_INITIAL_DATA = {
   "appName": "Múzy v chůzi",
   "source": "Chození 2026.xlsx",
-  "generatedAt": "2026-07-28T10:30:31",
+  "generatedAt": "2026-07-28T10:32:46",
   "routes": [
     {
       "id": "2026-06-10",
